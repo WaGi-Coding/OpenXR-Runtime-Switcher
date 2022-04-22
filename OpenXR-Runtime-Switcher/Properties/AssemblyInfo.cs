8d64b4a377dc6eb947df21a27cf83416279dc48f
Taki7o7 <wagi.coding@gmail.com>
2022-04-22 08:53:15 +0200

    update version info

--- OpenXR-Runtime-Switcher/Properties/AssemblyInfo.cs
+++ OpenXR-Runtime-Switcher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ef8c31e1-dde3-4dec-a36d-d9475ea703d4")]
 
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
